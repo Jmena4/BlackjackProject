ad41a804018c23bf28aab9f4226cdd9b18bf37f5
Jesse <Jesse.mena4@gmail.com>
2019-01-26 14:32:34 -0700

    Update: changes to the BlackjackHand and BlackjackHandSim objects

--- src/com/skilldistillery/cardgames/blackjack/BlackjackHand.java
+++ src/com/skilldistillery/cardgames/blackjack/BlackjackHand.java
@@ -1,13 +1,19 @@
 package com.skilldistillery.cardgames.blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.skilldistillery.cardgames.common.Card;
 import com.skilldistillery.cardgames.common.Hand;
 
 public class BlackjackHand extends Hand {
+	private List<Card> blackjackDeck;
 
 	public BlackjackHand() {
+		blackjackDeck = new ArrayList<>();
 		
-		getHandValue();
+		
 	}
-
 
 }
--- src/com/skilldistillery/cardgames/blackjack/BlackjackHandSim.java
+++ src/com/skilldistillery/cardgames/blackjack/BlackjackHandSim.java
@@ -1,23 +1,41 @@
 package com.skilldistillery.cardgames.blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import com.skilldistillery.cardgames.common.Card;
+import com.skilldistillery.cardgames.common.Deck;
 public class BlackjackHandSim {
+	private List<Card> blackjackGame = new ArrayList<>();
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		BlackjackHandSim app = new BlackjackHandSim();
+ 		BlackjackHandSim app = new BlackjackHandSim();
 		app.run();
 
 	}
 
 	public void run() {
-		Scanner sc = new Scanner(System.in);
 		runBlackjackHand(sc);
 	}
 
 	public void runBlackjackHand(Scanner sc) {
-		BlackjackHand(sc);
 		
+		Deck blackjackDeck = new Deck();
+		blackjackDeck.shuffle();
+		BlackjackHand playerHand = new BlackjackHand();
+		BlackjackHand dealerHand = new BlackjackHand();
+		String input= "";
+		do {
+			System.out.println("Would you like to play of a game of BlackJack? type 'yes' or 'no' to continue." );
+			input = sc.next();
+			System.out.println(input);
+			playerHand.addCard(blackjackDeck.dealCard());
+			playerHand.getHandValue();
+
+			sc.close();
+		} while (input != "no");
 		sc.close();
 	}
 
