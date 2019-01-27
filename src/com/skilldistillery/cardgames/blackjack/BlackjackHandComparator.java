908445d8894b55805f744ade12d9e5fb3228f6a8
Jesse <Jesse.mena4@gmail.com>
2019-01-26 23:57:16 -0700

    Update: Created a blackjack comparator class to compare the dealer and player scores

---
+++ src/com/skilldistillery/cardgames/blackjack/BlackjackHandComparator.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.cardgames.blackjack;
+
+import java.util.Comparator;
+
+public class BlackjackHandComparator implements Comparator<BlackjackHandSim> {
+
+	public BlackjackHandComparator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int compare(BlackjackHandSim o1, BlackjackHandSim o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
