7dd75f7c49a762338e859a3168180897118a7e4b
Jesse <Jesse.mena4@gmail.com>
2019-02-01 12:48:51 -0700

    Style: removed several unused lines of code

--- src/com/skilldistillery/cardgames/blackjack/BlackjackHand.java
+++ src/com/skilldistillery/cardgames/blackjack/BlackjackHand.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 import com.skilldistillery.cardgames.common.Hand;
 
 public class BlackjackHand extends Hand {
-	private int choice = 0;
 	Scanner sc = new Scanner(System.in);
 
 	public void runBlackjackHand() {
@@ -13,23 +12,6 @@ public class BlackjackHand extends Hand {
 //		BlackjackPlayer p = new BlackjackPlayer();
 //		BlackjackDealer d = new BlackjackDealer();
 
-////		Deal first card to player and dealer
-//		drawBlackjackHand(p, d);
-////		Check if either participant has blackjack
-//		gotBlackjack(p, d);
-////		Player can either hit or stand
-//		playerHitOrStand(p);
-////		Check player hand bust
-//		checkPlayerHandBust(p);
-////		Check dealer hand bust
-//		dealerHitOrStand(d);
-////		Check dealer bust
-//		checkDealerHandBust(d);
-////		Compare end hands, highest score wins
-//		blackjackWinner(p, d);
-////		Prompts the player to continue playing
-//		continueGame(p, d);
-////		System.out.println(d.getHandValue()); // for Testing blackjack win on dealer
 	}
 
 }
